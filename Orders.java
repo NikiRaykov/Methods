644649bde20b80cc4799c2580d786b09d52d4fcc
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2026-02-19 14:43:54 +0100

    Add files via upload

---
+++ Orders.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Orders {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String product = scanner.nextLine();
+        int quantity = Integer.parseInt(scanner.nextLine());
+
+        orderCalculate(product, quantity);
+    }
+
+    public static void orderCalculate(String product, int quantity) {
+        if (product.equals("coffee")) {
+            System.out.printf("%.2f", quantity * 1.50);
+        } else if (product.equals("water")) {
+            System.out.printf("%.2f", quantity * 1.00);
+        } else if (product.equals("coke")) {
+            System.out.printf("%.2f", quantity * 1.40);
+        } else if (product.equals("snacks")) {
+            System.out.printf("%.2f", quantity * 2.00);
+        }
+    }
+}
